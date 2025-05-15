1295d492620c5b77356aa62b8f0930c470b47136
vedanth515 <vedanthkarnataka@gmail.com>
2025-05-15 12:45:58 +0530

    Added core feature components: Categories, BestSeller, ProductCart, and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
+import {Toaster} from "react-hot-toast"
+import Footer from './Components/Fotter'
+import { useAppContext } from './Context/AppContext'
+import Login from './Components/Login'
+import AllProducts from './Pages/AllProducts'
+import ProductCategroy from './Pages/ProductCategroy'
+import ProductDetails from './Pages/ProductsDetails'
+import Cart from './Pages/Cart'
+import AddAddress from './Pages/AddAddress'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes("seller");
+
+  const {showUserLogin}=useAppContext()
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {isSellerPath ? null : <Navbar />}
+
+      {showUserLogin ? <Login/> : null }
+
+
+      <Toaster/>
+
+      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:24 xl:32"}`}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategroy />} />
+          <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />} />
+
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     { !isSellerPath && <Footer/>}
+    </div>
   )
 }
 
