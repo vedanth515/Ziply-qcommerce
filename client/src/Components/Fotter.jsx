80f4b26abcdfc1ce6c3a89dcc49ad4275d2e03db
vedanth515 <vedanthkarnataka@gmail.com>
2025-05-24 17:40:57 +0530

    Set upped the Express server with folder structure and MongoDB configuration

--- client/src/Components/Fotter.jsx
+++ client/src/Components/Fotter.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
-                Copyright {new Date().getFullYear()} © VedanthStack.dev All Right Reserved.
+                Copyright {new Date().getFullYear()} © ZiplyStack.dev All Right Reserved.
             </p>
         </div>
     );
---
+++ server/Server.js
@@ -0,0 +1,21 @@
+import cookieParser from "cookie-parser";
+import express from "express"
+import cors from "cors"
+
+const app=express();
+const port = process.env.PORT || 4000;
+
+//Allow multiple origins
+const allowedOrigins = ['http://localhost:5173']
+
+// Middleware Configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin:allowedOrigins,credentails:true}));
+
+app.get('/',(req,res)=>res.send("API is Working!"));
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    
+})
