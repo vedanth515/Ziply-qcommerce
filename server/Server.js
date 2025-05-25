9bfe70bcf7bba004bfc8850ff522c876f577cff3
vedanth515 <vedanthkarnataka@gmail.com>
2025-05-25 17:29:19 +0530

    Added the  seller authentication flow: login, logout, is-auth routes with controller and middleware

--- server/Server.js
+++ server/Server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import connectDB from "./configs/db.js";
 import 'dotenv/config';
 import userRouter from "./routes/UserRoute.js";
+import sellerRouter from "./routes/sellerRoute.js";
 
 const app=express();
 const port = process.env.PORT || 4000;
@@ -22,6 +23,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get('/',(req,res)=>res.send("API is Working!"));
 app.use('/api/user',userRouter);
+app.use('/api/seller',sellerRouter);
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
